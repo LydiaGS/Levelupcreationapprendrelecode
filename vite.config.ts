bb4732712b0ed5bee1f83365bf9a86c9a3f34c1e
amandine letellier <letellier.ama@gmail.com>
2026-03-15 02:21:09 +0100

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/Editeurlevelupcreation/",
+  base: "Levelupcreationapprendrelecode",
 
   plugins: [react(), tailwindcss(), viteSingleFile()],
 
